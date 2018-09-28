63eae54dc7f62d5808a2a5c4f1e6cc93c885d272
Marylene Chan <marylenechan@Marylenes-MacBook-Pro-2.local>
2018-09-28 17:48:12 -0500

    db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,6 @@
 cindy = User.create(first_name: "Cindy", last_name: "Vo", username: "Cindy", password: "password1")
 kelly = User.create(first_name: "Kelly", last_name: "Lwakatare", username: "Kelly", password: "password2")
 marylene = User.create(first_name: "Marylene", last_name: "Chan", username: "Marylene", password: "password3")
+cart1 = Cart.create(user_id: 1)
+cart2 = Cart.create(user_id: 2)
+cart3 = Cart.create(user_id: 3)
